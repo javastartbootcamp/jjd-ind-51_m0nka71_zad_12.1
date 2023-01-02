447a598138405cf511e730b75c51b36883fd978d
Monika Dabrowska <monikad71@onet.pl>
2023-01-02 14:09:57 +0100

    Solution 12.1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    static Math[] readFile(String fileName) throws FileNotFoundException {
-        final int linesNumber = countLines(fileName);
-        Math[] equations = new Math[linesNumber];
-        try (Scanner scan = new Scanner(new File(fileName))) {
-            for (int i = 0; i < equations.length; i++) {
+    private double firstNumber;
+    private String mathSing;
+    private double secondNumber;
 
-            }
+    public Calculator(double firstNumber, String mathSing, double secondNumber) {
+        this.firstNumber = firstNumber;
+        this.mathSing = mathSing;
+        this.secondNumber = secondNumber;
+    }
+
+    static Calculator[] readFile(String fileName) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(fileName));
+        int lines = countLines(fileName);
+        Calculator[] result = new Calculator[lines];
+        for (int i = 0; i < lines; i++) {
+            String line = scan.nextLine();
+            String[] split = line.split(" ");
+            double firstNumber = Double.parseDouble(split[0]);
+            String sign = split[1];
+            double secondNumber = Double.parseDouble(split[2]);
+            result[i] = new Calculator(firstNumber, sign, secondNumber);
         }
-        return equations;
+        return result;
     }
 
     private static int countLines(String fileName) throws FileNotFoundException {
@@ -24,5 +38,24 @@ public class Calculator {
             }
         }
         return lines;
+    }
+
+    static void calculateValues(Calculator[] calculators) {
+        double result = 0;
+        for (Calculator calculator : calculators) {
+            if (calculator.mathSing.equals("+")) {
+                result = calculator.firstNumber + calculator.secondNumber;
+                System.out.println(calculator.firstNumber + " + " + calculator.secondNumber + " = " + result);
+            } else if (calculator.mathSing.equals("-")) {
+                result = calculator.firstNumber - calculator.secondNumber;
+                System.out.println(calculator.firstNumber + " - " + calculator.secondNumber + " = " + result);
+            } else if (calculator.mathSing.equals("*")) {
+                result = calculator.firstNumber * calculator.secondNumber;
+                System.out.println(calculator.firstNumber + " * " + calculator.secondNumber + " = " + result);
+            } else if (calculator.mathSing.equals("/")) {
+                result = (calculator.firstNumber / calculator.secondNumber);
+                System.out.println(calculator.firstNumber + " + " + calculator.secondNumber + " = " + result);
+            }
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,26 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        FileReader fileReader = new FileReader("C:\\Users\\monik\\Desktop\\operations.txt");
-        BufferedReader reader = new BufferedReader(fileReader);
+        String fileName = "operations.txt";
+        File file = new File(fileName);
 
-//        String st = ("C:\\Users\\monik\\Desktop\\operations.txt");
-//        int resultOfAdding;
-//        for (int i = 0; i < st.length(); i++) {
-//            if (st.charAt(i) == '+') {
-//                resultOfAdding = Integer.parseInt(st.substring(0, i)) + Integer.parseInt(st.substring(i + 1));
-//                System.out.print(resultOfAdding);
-//            }
-//        }
- }
+        boolean fileExists = file.exists();
+        if (!fileExists) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Nie udało się utworzyć pliku");
+            }
+        }
+
+        FileReader fileReader = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fileReader);
+        Calculator[] calculators = Calculator.readFile(fileName);
+        Calculator.calculateValues(calculators);
+        reader.close();
+    }
 }
